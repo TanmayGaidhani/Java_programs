0a00c07213b9198843f0099e32d10436ab68e014
Tanmay Sanjay Gaidhani <tanmaygaidhani3@gmail.com>
2024-11-25 18:34:02 +0530

    Add files via upload

---
+++ Constructor1.java
@@ -0,0 +1,38 @@
+class Constructor{
+    private String name;
+    private int id;
+    private int salary;
+    public Constructor(){
+        System.out.println("Welcome........");
+    }
+    public Constructor(String myname,int myid,int mysalary){
+        name=myname;
+        id=myid;
+        salary=mysalary;
+        System.out.println("my name is:"+name+"\nmy id is:"+id+"\nmy salary is:"+salary);
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setid(int id){
+        this.id=id;
+    }
+    public int getid(){
+        return id;
+    }
+    public void setsalary(int salary){
+        this.salary=salary;
+    }
+    public int getsalary(){
+        return salary;
+    }
+}
+public class Constructor1{
+    public static void main(String[] args) {
+        Constructor y=new Constructor();
+        Constructor y1=new Constructor("tanmay sanjay gaidhani",113,234235);
+    }
+}
